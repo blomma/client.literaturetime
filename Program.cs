49d90b93dace3696fdcba71afc94891eec4a5e5a
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-08-24 18:43:17 +0200

    Removed https redirection and hst

--- Program.cs
+++ Program.cs
@@ -5,12 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 var jsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
 
@@ -38,7 +32,6 @@ app.MapGet("/literaturetime/{hour}/{minute}", async ([FromServices] HttpClient h
 })
 .WithName("GetLiteratureTime");
 
-// app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
