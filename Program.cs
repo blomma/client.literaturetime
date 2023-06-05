91c2d5965a7d66b072b4133ac41c331f80e32191
Mikael Hultgren <blomma@artsoftheinsane.com>
2023-06-05 20:12:41 +0200

    Use YARP to proxy request downstream

--- Program.cs
+++ Program.cs
@@ -1,20 +1,17 @@
-using Client.LiteratureTime.Exceptions;
-using Client.LiteratureTime.Handlers;
-using Client.LiteratureTime.Models;
 using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
 using System.Text.Json;
 
 Log.Logger = new LoggerConfiguration().MinimumLevel
-    .Override("Microsoft", LogEventLevel.Information)
+    .Override("Microsoft", LogEventLevel.Warning)
     .Enrich.FromLogContext()
     .WriteTo.Console()
     .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpForwarder();
 
 builder.Services.AddResponseCompression(options =>
 {
@@ -26,21 +23,6 @@ builder.Host.UseSerilog(
     (context, services, configuration) =>
         configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services)
 );
-
-builder.Services
-    .AddHttpClient(
-        Options.DefaultName,
-        client =>
-        {
-            client.BaseAddress = new Uri(
-                builder.Configuration.GetConnectionString("api.literaturetime")
-                    ?? throw new Exception()
-            );
-        }
-    )
-    .AddHttpMessageHandler<ProblemDetailsHandler>();
-
-builder.Services.AddMvcCore();
 
 builder.Services.AddHttpLogging(logging =>
 {
@@ -57,56 +39,16 @@ builder.Services.AddHttpLogging(logging =>
     logging.ResponseBodyLogLimit = 4096;
 });
 
-builder.Services.AddTransient<ProblemDetailsHandler>();
-
 var app = builder.Build();
 app.UseHttpLogging();
 app.UseResponseCompression();
 
 var jsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
 
-app.MapGet(
-        "/api/literature/{hour}/{minute}/{hash?}",
-        async (
-            CancellationToken cancellationToken,
-            [FromServices] HttpClient httpClient,
-            [FromServices] IConfiguration configuration,
-            string hour,
-            string minute,
-            string? hash
-        ) =>
-        {
-            var requestUrl =
-                hash != null
-                    ? $"/api/literature/{hour}/{minute}/{hash}"
-                    : $"/api/literature/{hour}/{minute}";
-
-            var response = await httpClient.GetAsync(requestUrl, cancellationToken);
-
-            using var contentStream = await response.Content.ReadAsStreamAsync(cancellationToken);
-            var literaturetime = await JsonSerializer.DeserializeAsync<LiteratureTime>(
-                contentStream,
-                jsonOptions,
-                cancellationToken
-            );
-
-            return Results.Ok(literaturetime);
-        }
-    )
-    .WithName("GetLiteratureTime")
-    .AddEndpointFilter(
-        async (invocationContext, next) =>
-        {
-            try
-            {
-                return await next(invocationContext);
-            }
-            catch (ManagedresponseException ex)
-            {
-                return Results.Problem(ex.ProblemDetails);
-            }
-        }
-    );
+app.MapForwarder(
+    "/api/literature/{hour}/{minute}",
+    builder.Configuration.GetConnectionString("api.literaturetime")!
+);
 
 app.UseStaticFiles(
     new StaticFileOptions
