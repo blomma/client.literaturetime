d00bf7e1e71b691811c2c2b777019029a13b5cef
Mikael Hultgren <blomma@artsoftheinsane.com>
2023-09-16 09:39:47 +0200

    Add aria-label to link

--- ClientApp/src/Footer.tsx
+++ ClientApp/src/Footer.tsx
@@ -6,7 +6,10 @@ export const Footer = () => {
     return (
         <div className="styledFooterContainer">
             <footer>
-                <a href="https://github.com/blomma/client.literaturetime">
+                <a
+                    href="https://github.com/blomma/client.literaturetime"
+                    aria-label="code at github"
+                >
                     <SiGithub title="github" />
                 </a>
             </footer>
